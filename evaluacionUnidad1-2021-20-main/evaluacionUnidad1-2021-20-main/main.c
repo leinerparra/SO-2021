70b6802ed86815ca0cd33af0510ec08904a24750
leinerparra <73318865+leinerparra@users.noreply.github.com>
2021-09-01 13:24:40 -0500

    FEATURE 8 OK
    FEATURE 8 TERMINADA Y TESTEADA QUEDA OK

--- evaluacionUnidad1-2021-20-main/evaluacionUnidad1-2021-20-main/main.c
+++ evaluacionUnidad1-2021-20-main/evaluacionUnidad1-2021-20-main/main.c
@@ -1,8 +1,57 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include "student.h"
 
 int main(int argc, char *argv[]){
 
+    if(argc != 3){
+        printf("number of arguments is not 3\n");
+        return(EXIT_FAILURE);
+    }
+
+    FILE *fin = fopen(argv[1],"r");
+    if (fin == NULL){
+        perror("open fin file fails: ");
+        return(EXIT_FAILURE);
+    }
+
+    FILE *fout = fopen(argv[2],"w");
+    if (fout == NULL){
+        perror("open fout file fails: ");
+        return(EXIT_FAILURE);
+    }
+
+    feature1(fin,fout);
+    feature2(fin,fout);
+    feature3(fin,fout);
+
+    int *parr=NULL ;
+    int length = 0;
+    char *op=NULL ;
+    feature4(fin,&parr,&length,&op);
+    feature5(fout, parr, length, op);
+
+    struct Obj_t obj = {"test",123};
+    feature6(fin, &obj);
+    feature7(fout,&obj);
+
+    struct _courseInfo_t *pobj;
+    int objArrSize = 0;
+    feature8(fin, &pobj, &objArrSize);
+    feature9(fout, pobj,objArrSize);
+   
+    // feature10: release memory
+/*
+    free(parr);
+    free(op);
+    free(obj.nombre);
+    free(pobj);
+
+    fclose(fin);
+    fclose(fout);
+*/
+
     return EXIT_SUCCESS;
+
 }
--- evaluacionUnidad1-2021-20-main/evaluacionUnidad1-2021-20-main/student.h
+++ evaluacionUnidad1-2021-20-main/evaluacionUnidad1-2021-20-main/student.h
@@ -2,10 +2,17 @@
 #define _STUDENT_H
 
 #include <stdio.h>
+#include <stdlib.h>
 
 struct Obj_t{
     char *nombre;
     int cedula;
+};
+
+struct _courseInfo_t{
+    char name[32];
+    int credits;
+    float grade;
 };
 
 void feature1(FILE *fin, FILE *fout);
@@ -15,7 +22,7 @@ void feature4(FILE *fin, int **parr, int *length, char **op);
 void feature5(FILE *fout, int *parr, int length, char *op);
 void feature6(FILE *fin, struct Obj_t *pobj);
 void feature7(FILE *fout, struct Obj_t *pobj);
-void feature8(FILE *fin, struct Obj_t **pobj,int *length);
-void feature9(FILE *fout, struct Obj_t *pobj,int length);
+void feature8(FILE *fin, struct _courseInfo_t **pobj,int *length);
+void feature9(FILE *fout, struct _courseInfo_t *pobj,int length);
 
 #endif
